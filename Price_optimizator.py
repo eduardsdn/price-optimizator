1d466798a3063bd69b08a088f54906f78980c741
Eduard Saadyan <eduard.sdn@gmail.com>
2019-05-29 13:20:00 +0300

    added pricelist text file processing function
    added pricelist text file processing function

--- Price_optimizator.py
+++ Price_optimizator.py
@@ -18,3 +18,17 @@ with open('PSStore_sale.json', 'r', encoding='utf-8') as f:
 
 
 print(f"Sales of PS4 games in PS Store: {catalog2}")
+
+def read_data_store(file_path): #reads pricelist.txt
+    data = []
+    with open(file_path, "r") as f:
+        f.readline()
+        for line in f:
+            parts = line.split(",")
+            row = [parts[0], parts[1],int(parts[2])]
+
+            data.append(row)
+
+    return data
+
+pricelist = read_data_store("Price_optimizator.txt") #matrix with pricelist
