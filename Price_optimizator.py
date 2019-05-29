114a9318b160ea23c310876c83f2ce0a00daab21
GlebFang <bunia200480@mail.ru>
2019-05-29 10:56:11 +0300

    Create Price_optimizator.py
    Testing out for github

---
+++ Price_optimizator.py
@@ -0,0 +1,12 @@
+def read_data(file_name):
+    data = []
+        f = open(file_name)
+        f.read()
+    return data
+
+
+data = read_data('Price_optimizator.txt')
+for i in range(2):
+    print(data[i])
+
+encoding
